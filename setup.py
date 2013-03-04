c673e28be1a0eb69ef1358a2df7861f021d75f2f
Arthur <arthurstreet@yahoo.com>
2013-03-04 21:21:35 +1100

    Added URL for github to setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'cmsplugin-rt',
-    version = '0.3.0',
+    version = '0.3.1',
     packages = find_packages(),   #'cmsplugin_rt',   #find_packages(),
     include_package_data = True,
     license = 'BSD License', # example license
-    description = 'This package contains a number of basic plugins to kick start your DjangoCMS project, such as Twitter Bootstrap navbar and buttons, Facebook and Twitter buttons, a Style Modifier, Google Analytics tracking code, Google fonts, meta tags and resizeable pictures.',
+    description = 'This package contains a number of basic plugins to kick start your DjangoCMS project, such as Twitter Bootstrap navbar and buttons, Facebook and Twitter buttons, a Style Modifier, Google Analytics tracking code, Google fonts, meta tags and resizable pictures.',
     long_description = README,
     keywords = "button meta twitter bootstrap style modifier racing tadpole",
-    url = 'http://racingtadpole.com',
+    url = 'https://github.com/RacingTadpole/cmsplugin-rt',
     author = 'Art Street',
     author_email = 'art@racingtadpole.com',
     classifiers = [
