b88a925c5d45fc6ab677b88806f44cb6b31a613e
Arthur Street <arthurstreet@yahoo.com>
2013-07-13 10:58:34 +1000

    If only one item in the carousel, do not show fwd and back buttons

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'cmsplugin-rt',
-    version = '0.4.8',
+    version = '0.4.9',
     packages = find_packages(),   #'cmsplugin_rt',   #find_packages(),
     include_package_data = True,
     license = 'BSD License', # example license
